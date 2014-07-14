3c0a634234e6c56f084c3b367a76e4c00d481b1e
Pongkemon01 <chief_robin@hotmail.com>
2014-07-14 22:32:48 +0700

    Basic implementation waiting to debug
    This is just a rough implementation without any validation of operation.

---
+++ README.txt
@@ -0,0 +1,26 @@
+The student-identity check quiz access rule
+
+This quiz access rule was created by Akrapong Patchararungruang of Kasetsart University. 
+It can be used with versions 2.2 of Moodle, or later.
+
+If you install this plugin, there are new options on the quiz settings form. The first
+is the option allow teacher to turn indentity check on and off. The second is the
+text that the teacher wants to display as the instruction for student to answer.
+The last is the minimum word count of a student answer.
+
+If the teacher turns identity-check on, then when a student tries to start a quiz attempt,
+they will see the instruction and an empty text input, and they will have to
+type something with the total words at least the previously set minimum before they are 
+allowed to start the quiz attempt.
+
+To install using git, type this command in the root of your Moodle install
+    git clone git://github.com/pongkemon01/moodle-quizaccess_studentident.git mod/quiz/accessrule/studentident
+    echo '/mod/quiz/accessrule/studentident/' >> .git/info/exclude
+
+Alternatively, download the zip from
+    https://github.com/pongkemon01/moodle-quizaccess_studentident/zipball/master
+unzip it into the mod/quiz/accessrule folder, and then rename the new
+folder to 'studentident'.
+
+Once installed you need to go to the Site administration -> Notifications page
+to let the plugin install itself.
---
+++ rule.php
@@ -0,0 +1,174 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Implementaton of the quizaccess_honestycheck plugin.
+ *
+ * @package   quizaccess_studentident
+ * @copyright 2014 Kasetsart University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/quiz/accessrule/accessrulebase.php');
+
+
+/**
+ * A rule requiring the student to promise not to cheat.
+ *
+ * @copyright  2011 The Open University
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class quizaccess_studentident extends quiz_access_rule_base {
+
+    public function add_preflight_check_form_fields(mod_quiz_preflight_check_form $quizform,
+            MoodleQuickForm $mform, $attemptid) {
+
+        $mform->addElement('header', 'studentidentheader',
+                get_string('studentidentheader', 'quizaccess_studentident'));
+        $mform->addElement('static', 'studentidentcaption', '',
+				$this->quiz->studentidentcaption);
+        $mform->addElement('textarea', 'studentident', '',
+				array('rows' => 2, 'cols' => 70));
+		$mform->setType('studentident', PARAM_RAW_TRIMMED);
+    }
+	
+	public function compute_ident_key($data) {
+		return $key;
+	}
+
+    public function validate_preflight_check($data, $files, $errors, $attemptid) {
+		global $DB;
+        if (empty($data['studentident'])) {
+            $errors['studentident'] = get_string('youmustpass', 'quizaccess_studentident');
+			return $error;
+        }
+		
+		# == Clean up phase ==
+		# split the phrase by any number of commas or space characters,
+		# which include " ", \r, \t, \n and \f
+		$identwords = preg_split("/[\s,]+/", $data['studentident']);
+		# Join the split phrase back without adding any commas
+		$ident = implode (' ', $identwords);
+		# Finally, convert all char to lower case
+		$ident = strtolower ($ident);
+		
+		# == Checking phase ==
+		# Minimum word count checking
+		if (count ($identwords) < intval ($quiz->studentidentmin)) {
+			$errors['studentident'] = get_string('youmustpass', 'quizaccess_studentident');
+			return $error;
+		}
+		$identkey = compute_ident_key ($ident);
+		# Search for existing ident in DB
+		$record = $DB->get_record('quizaccess_studentident_idents', array('ident' => $identkey));
+        if (!$record) {
+			# Not found. Student have passed the identity check. Store new identkey to the database.
+            $record->quizid = $quiz->id;
+            $record->ident = $identkey;
+            $DB->insert_record('quizaccess_studentident_ident', $record);
+		} else {
+			$errors['studentident'] = get_string('youmustpass', 'quizaccess_studentident');
+		}
+
+        return $errors;
+    }
+	
+	public function is_preflight_check_required($attemptid) {
+		global $SESSION;
+		return empty($SESSION->passidentcheckedquizzes[$this->quiz->id]);
+	}
+
+	public function notify_preflight_check_passed($attemptid) {
+        global $SESSION;
+        $SESSION->passidentcheckedquizzes[$this->quiz->id] = true;
+    }
+    
+    public function current_attempt_finished() {
+        global $SESSION;
+        # Clear the flag in the session that says that the user has already
+        # entered the password for this quiz.
+        if (!empty($SESSION->passidentcheckedquizzes[$this->quiz->id])) {
+            unset($SESSION->passidentcheckedquizzes[$this->quiz->id]);
+        }
+    }
+
+    public static function make(quiz $quizobj, $timenow, $canignoretimelimits) {
+
+        if (empty($quizobj->get_quiz()->studentidentrequired)) {
+            return null;
+        }
+
+        return new self($quizobj, $timenow);
+    }
+
+    public static function add_settings_form_fields(
+            mod_quiz_mod_form $quizform, MoodleQuickForm $mform) {
+		
+		$mform->addElement('checkbox', 'studentidentrequired', 
+				get_string('studentidentrequired', 'quizaccess_studentident'));
+		$mform->addElement('text', 'studentidentcaption', 
+				get_string('studentidentcaption', 'quizaccess_studentident'), '');
+		$mform->addElement('text', 'studentidentmin', 
+				get_string('studentidentmin', 'quizaccess_studentident'), 'size="3"');
+
+		$mform->setType('studentidentcaption', PARAM_RAW_TRIMMED);
+		$mform->setType('studentidentmin', PARAM_INTEGER);
+		
+		$mform->addHelpButton('studentidentrequired',
+                'studentidentrequired', 'quizaccess_studentident');
+	}
+
+	# We should write the function 'public static function validate_settings_form_fields'
+	# to validate given setting
+
+    public static function save_settings($quiz) {
+        global $DB;
+        if (empty($quiz->studentidentrequired)) {
+            $DB->delete_records('quizaccess_studentident', array('quizid' => $quiz->id));
+        } else {
+            $record = $DB->get_record('quizaccess_studentident', array('quizid' => $quiz->id));
+            if (!$record) {
+                $record = new stdClass();
+                $record->quizid = $quiz->id;
+                $record->studentidentrequired = 1;
+				$record->studentidentcaption = $quiz->studentidentcaption;
+				$record->studentidentmin = intval ($quiz->studentidentmin);
+                $DB->insert_record('quizaccess_studentident', $record);
+            } else {
+                $record->studentidentrequired = 1;
+				$record->studentidentcaption = $quiz->studentidentcaption;
+				$record->studentidentmin = intval ($quiz->studentidentmin);
+                $DB->update_record('quizaccess_studentident', $record);
+            }
+        }
+    }
+
+    public static function delete_settings($quiz) {
+        global $DB;
+        $DB->delete_records('quizaccess_studentident', array('quizid' => $quiz->id));
+		$DB->delete_records('quizaccess_studentident_idents', array('quizid' => $quiz->id));
+    }
+
+    public static function get_settings_sql($quizid) {
+        return array(
+            'studentidentrequired, studentidentcaption, studentidentmin',
+            'LEFT JOIN {quizaccess_studentident} studentident ON studentident.quizid = quiz.id',
+            array());
+    }
+}
